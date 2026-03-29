b28a7f22c927e28a33c5a3440936a73c37e1e744
dletelieralvarez <dani.letelier@duocuc.cl>
2026-03-28 21:58:15 -0300

    ajustes

--- src/main/java/com/biblioteca/bff/config/HttpClientConfig.java
+++ src/main/java/com/biblioteca/bff/config/HttpClientConfig.java
@@ -2,7 +2,6 @@ package com.biblioteca.bff.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
